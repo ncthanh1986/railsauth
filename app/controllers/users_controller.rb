a91d2c2b27ede833601728a3dbdf6ae2dfc7a16a
Thanh Nguyen <tnguyenuni@gmail.com>
2017-01-13 00:15:24 +1100

    user new addeddd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,18 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "The user was added!"
+            redirect_to root_path
+        else 
+            render 'new'
+        end
     end
 
     private
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 
 end
